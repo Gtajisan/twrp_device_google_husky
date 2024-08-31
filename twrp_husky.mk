e8e21e06a5be3ee2c55a7510d4c376b1709e0870
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-08-31 22:26:11 +0600

    Update twrp_husky.mk

--- twrp_husky.mk
+++ twrp_husky.mk
@@ -9,10 +9,10 @@
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from generic device
-$(call inherit-product, device/google/husky/device.mk)
+$(call inherit-product, device/google/cheetah/device.mk)
 
-PRODUCT_DEVICE := husky
-PRODUCT_NAME := twrp_husky
+PRODUCT_DEVICE := cheetah
+PRODUCT_NAME := twrp_cheetah
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
 PRODUCT_MANUFACTURER := google
@@ -22,4 +22,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="husky-user 14 AP2A.240805.005 12025142 release-keys"
 
-BUILD_FINGERPRINT := google/husky/husky:14/AP2A.240805.005/12025142:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:14/AP2A.240805.005/12025142:user/release-keys
